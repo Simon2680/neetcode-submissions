ab55609dd400d6e24d80ebe0cb4eed9a6a7a842d
Simon Iradukunda <irasimon03@gmail.com>
2026-06-17 11:41:50 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+      #   for i in range(len(nums)):
+      #    for j in range(len(nums)):
+      #       if nums[i] + nums[j] == target and i != j:
+      #          return [i, j]
+
+      '''
+      create a hasmap
+      loop through the nums
+      if number-target is in hasmap and its value is different from index of number --> [value, index]
+      else:
+         add the nums-target to the hasmap and its corresponding index
+      '''
+      hasmap = {}
+      for i in range(len(nums)):
+         compliment = target - nums[i]
+         if compliment in hasmap and hasmap[compliment] != i:
+            return [hasmap[compliment], i]
+         else:
+            hasmap[nums[i]] = i
+      #return []
+        

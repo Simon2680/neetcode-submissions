c89bca8b8e82d581dc617176022c59682f1ebe18
Simon Iradukunda <irasimon03@gmail.com>
2026-06-18 07:54:09 -0400

    Update: anagram-groups - submission-1

--- Data Structures & Algorithms/anagram-groups/submission-1.py
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -1,5 +1,7 @@
+from collections import Counter
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
         """
         create a helper method to find count vector of each word
         return a tuple that will be the key in dictionary
@@ -16,33 +18,72 @@ class Solution:
 
           in the end return list of all. dictionary values
 
-          edge cases: when list is empty --> return [[""]]
+        #   edge cases: when list is empty --> return [[""]]
 
 
 
 
         
-        """
-        def find_count_vector(word):
-            countVector = [0]*26
-            for char in word:
-                countVector[ord(char)-ord("a")] +=1
+        # """
+        # def find_count_vector(word):
+        #     countVector = [0]*26
+        #     for char in word:
+        #         countVector[ord(char)-ord("a")] +=1
 
-            return tuple(countVector)
+        #     return tuple(countVector)
 
-        if len(strs) == 0:
-            return [[""]]
+        # #if len(strs) == 0:
+        #     #return [[""]]
         
 
-        myDict = {}
+        # myDict = {}
 
+        # for word in strs:
+        #     if find_count_vector(word) not in myDict:
+        #         myDict[find_count_vector(word)] = [word]
+        #     else:
+        #         myDict[find_count_vector(word)].append(word)
+
+        # return list(myDict.values())
+        #use a hashmap
+        # create a helper method to find a hashmap of a word
+
+        # use apointer to track when u are still in the same anagram
+
+        # def count_vector(word):
+        #     lst = [0]*26
+        #     for i in range(len(word)):
+        #         lst[ord(word[i])-ord('a')] += 1
+            
+        #     return tuple(lst)
+        # hasmap = {}
+        # for i in range(len(strs)):
+        #     if count_vector(strs[i]) not in hasmap:
+        #         hasmap[count_vector(strs[i])] = [strs[i]]
+        #     else:
+        #         hasmap[count_vector(strs[i])].append(strs[i])
+        # return list(hasmap.values())
+
+        red = defaultdict(list)
         for word in strs:
-            if find_count_vector(word) not in myDict:
-                myDict[find_count_vector(word)] = [word]
-            else:
-                myDict[find_count_vector(word)].append(word)
+            sortedS = "".join(sorted(word))
+            red[sortedS].append(word)
+        return list(red.values())
 
-        return list(myDict.values())
+
+
+    
+            
+
+
+
+
+            
+
+            
+        
+        
+
                 
 
         

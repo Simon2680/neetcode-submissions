d5d3c5409c3a13b521d13991f47d29d4b2531807
Simon Iradukunda <irasimon03@gmail.com>
2026-06-19 16:35:06 -0400

    Add: last-stone-weight - submission-9

---
+++ Data Structures & Algorithms/last-stone-weight/submission-9.py
@@ -0,0 +1,39 @@
+import heapq
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        '''
+        make a heap
+
+        while x and y:
+        
+        pop two max : x and y
+        compare them and if x==y:
+          do nothing since we have already poped them put
+        if x != y:
+            small stone is destroyed--> no need to push it back
+            bigger stone change weight:
+            push max-min
+
+        # Returns (x or y) if the condition evaluates to True, otherwise returns None
+          return (x or y) if (x or y) else None
+        '''
+        myheap = [-stone for stone in stones]
+        heapq.heapify(myheap)
+    
+        while len(myheap)>1:
+            x = -heapq.heappop(myheap)
+            y = -heapq.heappop(myheap)
+
+            if x != y:
+               diff = max(x,y) - min(x,y)
+               heapq.heappush(myheap, -1 *diff)
+
+        return -heapq.heappop(myheap) if myheap else 0
+            
+
+
+
+
+
+        
+        

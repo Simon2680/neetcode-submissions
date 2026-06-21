9864d25f9cb3a0ede9452c4c3e3ebe17fd25d4b3
Simon Iradukunda <irasimon03@gmail.com>
2026-06-20 21:08:54 -0400

    Update: longest-consecutive-sequence - submission-2

--- Data Structures & Algorithms/longest-consecutive-sequence/submission-2.py
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.py
@@ -16,26 +16,59 @@ class Solution:
 
         """
 
-        counter = 1
-        nums.sort()
-        count = 1
+        # counter = 1
+        # nums.sort()
+        # count = 1
 
-        i = 0
-        if len(nums) == 0:
-            return 0
-        while i < len(nums)-1:
+        # i = 0
+        # if len(nums) == 0:
+        #     return 0
+        # while i < len(nums)-1:
             
-            if (nums[i] - nums[i+1]) == -1:
-                count+=1
-            elif (nums[i] - nums[i+1]) == 0:
-                count+=0
-            else:
-                counter = max(counter, count)
-                count = 1
-            i += 1
-        counter = max(counter, count)
+        #     if (nums[i] - nums[i+1]) == -1:
+        #         count+=1
+        #     elif (nums[i] - nums[i+1]) == 0:
+        #         count+=0
+        #     else:
+        #         counter = max(counter, count)
+        #         count = 1
+        #     i += 1
+        # counter = max(counter, count)
         
-        return counter
+        # return counter
+        """
+        create a hashmap that will keep array of all the numbers that are consecutive
+        look for the longest value array
+        return the longest vakue array
+
+        sort the list
+        add alement in the hasmap
+        check  if  a number is there, if it is there , skip it
+        pointers
+        if it is not: 
+
+        """
+        myset = set(nums)
+        ln = 0
+        mylist = list(myset)
+        mylist.sort()
+
+        if len(mylist) >= 1:
+            mylist.append(mylist[-1]) 
+        else:
+            return len(mylist)
+        pointer = 0
+       
+        for i in range(len(mylist)-1):
+
+            if mylist[i+1] - mylist[i] != 1:
+                ln = max(ln, i-pointer +1)
+                pointer =i+1
+        
+        return ln
+
+        
+
             
 
 

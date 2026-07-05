1c6e9154dda0d84d6b4726bcf9034794d2880bda
Simon Iradukunda <irasimon03@gmail.com>
2026-07-05 09:55:06 -0400

    Update: two-integer-sum-ii - submission-1

--- Data Structures & Algorithms/two-integer-sum-ii/submission-1.py
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.py
@@ -1,30 +1,12 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        """
-        use pointers
-        use 2 pointers
-        one at the end and other at the begining
-        compare two numbers at both end
-        if sum is > target--> move right pointer in
-        if sum is < target--> move the right pointer in
-        if sum = target--> return [leftPointer, rightPointer]
 
+        R = len(numbers) -1
 
-        """
+        for L in range(len(numbers)):
+            while numbers[L] + numbers[R] > target:
+                R = R - 1
 
-        left = 0
-        right = len(numbers)-1
-        while True:
-            if numbers[left] + numbers[right] > target:
-                right -= 1
-            elif numbers[left] + numbers[right] < target:
-                left += 1
-            else:
-                return [left+1,right+1]
+            if numbers[L] + numbers[R] == target:
+                return [L+1, R+1]
 
-    
-       
-
-                
-                
-        

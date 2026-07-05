ed8a13085a97f1fdea9f99f6bd5d9fb4138df801
Simon Iradukunda <irasimon03@gmail.com>
2026-07-05 16:49:41 -0400

    Update: max-water-container - submission-0

--- Data Structures & Algorithms/max-water-container/submission-0.py
+++ Data Structures & Algorithms/max-water-container/submission-0.py
@@ -1,35 +1,19 @@
 class Solution:
     def maxArea(self, heights: List[int]) -> int:
-        """
-        area =0
-        use two pointers right and left at both ends
-        use a while loop that wil check if the pointers hasn't met yet
-        find the width --> right-left
-        find the height == min(height[right], height[left])
-        find area
-        update area if it's grater than the previous one--> use max
-        then: move the pointers:
-             move the one with height[pointer] is small
-             if they are equal, move any
 
-        return the area
+        maxArea = 0
 
-        """
-        area = 0
-        left = 0
-        right = len(heights)-1
-        while right > left:
-            # find width
-            width = right -left
-            height = min(heights[right], heights[left])
-            area = max(area, height * width)
-             # move pointers
-            if heights[right] > heights[left]:
-                left +=1
-            elif heights[right] < heights[left]:
-                right -= 1
+        l, r = 0, len(heights) - 1
+
+        while l < r:
+            detHeight = min(heights[l], heights[r])
+            area = (r - l) * detHeight
+            maxArea = max(maxArea, area)
+
+            if detHeight == heights[l]:
+                l += 1
             else:
-                left +=1
-        return area
+                r -= 1
+        return maxArea
 
         

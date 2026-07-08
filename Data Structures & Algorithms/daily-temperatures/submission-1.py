3bf9b061d21a73e2dfb8d4987212ce3d1bc174a1
Simon Iradukunda <irasimon03@gmail.com>
2026-07-07 21:07:06 -0400

    Update: daily-temperatures - submission-1

--- Data Structures & Algorithms/daily-temperatures/submission-1.py
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -1,48 +1,28 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = []
+        
+        # for i in range(len(temperatures)):
+        #     if (i+1) == len(temperatures):
+        #         res.append(0)
+        #         continue
+
+        #     counter = 1
+
+        #     while temperatures[i] > temperatures[i+counter]:
+        #         counter += 1
+        #         if i + counter == len(temperatures):
+        #             counter = 0
+        #             break
+        #     res.append(counter)
+
+        res = [0] * len(temperatures)
         stack = []
-        res = [0]*len(temperatures)
-        for i in range(len(temperatures)):
-            if not stack:
-                stack.append(i)
-            else:
-                if temperatures[stack[-1]] > temperatures[i]:
-                    stack.append(i)
-                else:
-                    while stack and temperatures[stack[-1]] < temperatures[i]:
-                        index = stack.pop()
-                        res[index] = i-index
-                    stack.append(i)
+
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                T, idx = stack.pop()
+                res[idx] = (i - idx)
+            stack.append([t, i])
+
         return res
-        
-    """
-    use stacks:
-    create an empty stack
-    create a res list
-    create a for loop that will pass through all the days:
-       if stack is empyty:
-          push the day[i] in stack:
-        else:
-            check if day[-1] from the stack is > day[i], if true:
-              push day[i] to the stack
-            if not:
-                have a while loop to go back and pop everything until day[i] < day[-1] from the stack: --> use a while loop
-                  if stack[-1] < days[i]:
-                    pop from stack
-                    add the distance into res:
-                      res[index(popped)] = index(days[i]) - index(popped) 
-                  after reaching where stack[-1] > day[i]:
-                    stop and push day[i]
-
-                    process continue
-        return res
-
-                   
-               
-                
-
-    """
-    
-
-
-        

61ebeeb7f00c34505f4a45af08df13170d02b03f
Simon Iradukunda <irasimon03@gmail.com>
2026-07-09 19:38:08 -0400

    Update: trapping-rain-water - submission-0

--- Data Structures & Algorithms/trapping-rain-water/submission-0.py
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.py
@@ -1,42 +1,21 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        """
-        o(n) and o(n) space solution
-        ##############################
-        water = 0
-        use a for loop that will  pass once at every number:
-          find max at the left
-          find max at the right
-          find maxG between (maxright, and maxleft)
-          find the maxG-cuurent height --> call it units
-            if units > 0:
-                water+=units
 
-        return water
-
-
-          
-        """
-        if len(height) < 2:
-            return 0
-
-        water = 0
+        final = 0
+        maxRight = [0] * len(height)
+        maxLeft = [0] * len(height)
+        for i in reversed(range(len(height) - 1)):
+            maxRight[i] = max(height[i+1],maxRight[i+1])
+        
+        for i in range(1,len(height)):
+            maxLeft[i] = max(height[i-1],maxLeft[i-1])
 
         for i in range(len(height)):
-            if i ==0:
-                leftmax =0
-                rightmax = max(height[i+1:])
+            final += max((min(maxRight[i], maxLeft[i]) - height[i]), 0)
 
-            elif i == len(height)-1:
-                rightmax = 0
-                leftmax = max(height[0:i])
+        # print(maxLeft)
+        # print(maxRight)
 
-            else:
-                leftmax = max(height[0:i])
-                rightmax = max(height[i+1:])
-            
-            Gmin = min(leftmax, rightmax)
-            units = Gmin-height[i]
-            if units>0:
-                water += units
-        return water
+        return final
+
+        

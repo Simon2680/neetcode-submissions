fc449c5c0d36b85577d0a47b07effb9395ac45d4
Simon Iradukunda <irasimon03@gmail.com>
2026-07-09 19:40:13 -0400

    Update: trapping-rain-water - submission-1

--- Data Structures & Algorithms/trapping-rain-water/submission-1.py
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.py
@@ -1,115 +1,19 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
 
-        """
-        o(n) and o(n) space solution
-        ##############################
-        water = 0
-        use a for loop that will  pass once at every number:
-          find max at the left
-          find max at the right
-          find maxG between (maxright, and maxleft)
-          find the maxG-cuurent height --> call it units
-            if units > 0:
-                water+=units
-
-        return water
-
-
-          
-        """
-        if len(height) < 2:
-            return 0
-
-        water = 0
-
-        for i in range(len(height)):
-            if i ==0:
-                leftmax =0
-                rightmax = max(height[i+1::])
-
-            elif i == len(height)-1:
-                rightmax = 0
-                leftmax = max(height[::i])
-
-            else:
-                leftmax = max(height[0:i])
-                rightmax = max(height[i+1::])
-            
-            Gmin = min(leftmax, rightmax)
-            units = Gmin-height[i]
-            if units>0:
-                water += units
-        return water
-        # ############################
-        # #O(n) solution
-        # """
-        # first find the left maxes and put them in the list: use o(n):
-        #   create a for loop:
-        #   left max of the first number is 0 
-        #   creaee a number that will keep the value of current max
-        #   compare the value to the new max and update the cuurent max:
+        final = 0
+        maxRight = [0] * len(height)
+        maxLeft = [0] * len(height)
+        for i in reversed(range(len(height) - 1)):
+            maxRight[i] = max(height[i+1],maxRight[i+1])
         
-        #  """
+        for i in range(1,len(height)):
+            maxLeft[i] = max(height[i-1],maxLeft[i-1])
+            final += max((min(maxRight[i], maxLeft[i]) - height[i]), 0)
 
-        # leftmaxes=[0]
-        # currmax = 0
-        # for i in range(len(height)):
-        #     if height[i]>currmax:
-        #         if i!=len(height):
-        #             leftmaxes.append(height[i])
-        #             currmax = height[i] 
-        #     else:
-        #         leftmaxes.append(currmax)
-        # rightmaxes =[]
-        # cmx = 0
-        # for i in range(len(height):-1):
+        # print(maxLeft)
+        # print(maxRight)
 
+        return final
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        

0e7cf85993bf1bfd1a3b35e98905007601e7693e
Simon Iradukunda <irasimon03@gmail.com>
2026-07-14 23:12:55 -0400

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,72 @@
+from collections import Counter
+from collections import defaultdict
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1hasmap = Counter(s1)
+        temps1 = defaultdict(int)
+        l = 0
+        for r in range(len(s2)):
+            if r-l < len(s1):
+                temps1[s2[r]] +=1
+            else:
+                if s1hasmap == temps1:
+                    return True
+                else:
+                    temps1[s2[l]] -=1
+                    if temps1[s2[l]] == 0:
+                        del temps1[s2[l]]
+                    l +=1
+                    temps1[s2[r]] +=1
+        return temps1 == s1hasmap
+                    
+                    
+                
+        
+        
+        
+        
+        
+        
+            
+                
+            
+                
+                    
+                
+                    
+                    
+                        
+                    
+        
+        
+        
+        
+    #     """
+      
+               
+    #           l               
+    #   s2 = "e i d b a o o o"
+    #               r  
+                   
+         
+        
+    #     """
+    #     """
+    #     create s1 hashmap ={a:1, b:1}
+    #     temps1 = {}
+    #     l = 0
+    #     for loop to move r:
+    #         if r-l < len(s1):
+    #             populate temps1[r]
+    #         else:
+    #             check if s1 == temps1
+    #             if True--> True
+    #             else:
+    #                 decrement temps1[l] by 1
+    #                 check if it the value is 0:
+    #                 if true--> remove the l in the temps1
+    #                 incerement l by 1
+    #     return false
+        
+    #     """
+       

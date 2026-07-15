45403f74c544b49ccdd3d5bb97a81d7f255b07ae
Simon Iradukunda <irasimon03@gmail.com>
2026-07-14 22:16:17 -0400

    Update: permutation-string - submission-0

--- Data Structures & Algorithms/permutation-string/submission-0.py
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -1,34 +1,72 @@
+from collections import Counter
+from collections import defaultdict
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
-
-        s1Count, s2Count = [0] * 26, [0] * 26
-        for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord('a')] += 1
-            s2Count[ord(s2[i]) - ord('a')] += 1
-
-        matches = 0
-        for i in range(26):
-            matches += (1 if s1Count[i] == s2Count[i] else 0)
-
+        s1hasmap = Counter(s1)
+        temps1 = defaultdict(int)
         l = 0
-        for r in range(len(s1), len(s2)):
-            if matches == 26:
-                return True
-
-            index = ord(s2[r]) - ord('a')
-            s2Count[index] += 1
-            if s1Count[index] == s2Count[index]:
-                matches += 1
-            elif s1Count[index] + 1 == s2Count[index]:
-                matches -= 1
-
-            index = ord(s2[l]) - ord('a')
-            s2Count[index] -= 1
-            if s1Count[index] == s2Count[index]:
-                matches += 1
-            elif s1Count[index] - 1 == s2Count[index]:
-                matches -= 1
-            l += 1
-        return matches == 26
+        for r in range(len(s2)):
+            if r-l < len(s1):
+                temps1[s2[r]] +=1
+            else:
+                if s1hasmap == temps1:
+                    return True
+                else:
+                    temps1[s2[l]] -=1
+                    if temps1[s2[l]] == 0:
+                        del temps1[s2[l]]
+                    l +=1
+                    temps1[s2[r]] +=1
+        return s1hasmap == temps1
+                    
+                    
+                
+        
+        
+        
+        
+        
+        
+            
+                
+            
+                
+                    
+                
+                    
+                    
+                        
+                    
+        
+        
+        
+        
+    #     """
+      
+               
+    #           l               
+    #   s2 = "e i d b a o o o"
+    #               r  
+                   
+         
+        
+    #     """
+    #     """
+    #     create s1 hashmap ={a:1, b:1}
+    #     temps1 = {}
+    #     l = 0
+    #     for loop to move r:
+    #         if r-l < len(s1):
+    #             populate temps1[r]
+    #         else:
+    #             check if s1 == temps1
+    #             if True--> True
+    #             else:
+    #                 decrement temps1[l] by 1
+    #                 check if it the value is 0:
+    #                 if true--> remove the l in the temps1
+    #                 incerement l by 1
+    #     return false
+        
+    #     """
+       

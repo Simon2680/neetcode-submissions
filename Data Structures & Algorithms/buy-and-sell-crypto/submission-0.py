c3b821df5ca7b471ec8a09bfef5a6f57b4bd7197
Simon Iradukunda <irasimon03@gmail.com>
2026-07-15 15:05:44 -0400

    Update: buy-and-sell-crypto - submission-0

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -1,11 +1,32 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        final = 0
-        maxSells = [0] * len(prices)
-        for i in reversed(range(len(prices) - 1)):
-            maxSells[i] = max(prices[i+1],maxSells[i+1])
-            final = max(final, maxSells[i] - prices[i])
-        
-        return final
+        """
+       
+ 
+            l
+        10  1  5  6  7 1
+                       r
+        create pointer l
+        create max_profit = 0
 
+        create a for loop to move r from 1 :
+        find profit
+        if profit >0:
+            continue
+            update profit
+        else:
+            l = r
+        return max_profit
+
+        """
+        max_profit = 0
+        l =0
+        for r in range(1, len(prices)):
+            profit = prices[r]-prices[l]
+            if profit >0:
+                max_profit = max(max_profit, profit)
+                continue
+            else:
+                l =r
+        return max_profit
         

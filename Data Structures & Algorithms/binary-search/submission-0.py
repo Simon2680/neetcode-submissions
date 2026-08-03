bbfe8213adf670c7e2ee429d548bf205b8a5560d
Simon Iradukunda <irasimon03@gmail.com>
2026-08-02 22:42:29 -0400

    Update: binary-search - submission-0

--- Data Structures & Algorithms/binary-search/submission-0.py
+++ Data Structures & Algorithms/binary-search/submission-0.py
@@ -1,15 +1,17 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums)-1
+    def bin_find(self, nums: List[int], start_idx:int, end_idx:int, target:int) -> int:
+        if start_idx >= end_idx:
+            return start_idx if start_idx < len(nums) and nums[start_idx] == target else -1
 
-        while right >= left:
-            mid = (left + right)//2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] > target:
-                right = mid -1
-            else:
-                left = mid +1
-        return -1
-        
+        mid_idx = (start_idx + end_idx) // 2
+
+        if nums[mid_idx] == target:
+            return mid_idx
+
+        if nums[mid_idx] > target:
+            return self.bin_find(nums, start_idx, mid_idx - 1, target)
+        else:
+            return self.bin_find(nums, mid_idx + 1, end_idx, target)
+
+    def search(self, nums: List[int], target: int) -> int:
+        return self.bin_find(nums,0,len(nums) - 1, target)

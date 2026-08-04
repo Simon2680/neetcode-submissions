77686c4803c9901c8d5ab9a753994f10e24a5f32
Simon Iradukunda <irasimon03@gmail.com>
2026-08-04 16:55:35 -0400

    Add: design-twitter-feed - submission-2

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-2.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+import heapq
+class Twitter:
+
+    def __init__(self):
+        self.timestamp = 0
+        self.tweetmap = defaultdict(list) #userid and tweetids
+        self.followersmap = defaultdict(set) # userd and followees id
+        
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        #add the tweet in the list corresponding to its userid and attach the timestamp on it
+        self.timestamp -=1
+        heapq.heappush(self.tweetmap[userId],(self.timestamp, tweetId))
+        
+    def getNewsFeed(self, userId: int) -> List[int]:
+        #get all the followees
+        alltweets= list(self.tweetmap[userId])
+        for followeeId in self.followersmap[userId]:
+            #combine all tweets
+            if followeeId != userId:
+                alltweets.extend(self.tweetmap[followeeId])
+            
+        heapq.heapify(alltweets)
+        res = []
+        for _ in range(min(10, len(alltweets))):
+            res.append(heapq.heappop(alltweets)[1])
+        return res
+
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        #add follow id into its corresponding userid values in followers map
+        if followeeId not in self.followersmap[followerId]:
+            self.followersmap[followerId].add(followeeId) 
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        #remove the followee from the set
+        if followeeId in self.followersmap[followerId]:
+            self.followersmap[followerId].remove(followeeId)
+
+        
